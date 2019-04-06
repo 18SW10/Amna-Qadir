4fd8d6598f7977470629ee6e4d2dbaa25f9676e1
18SW10 <49333095+18SW10@users.noreply.github.com>
2019-04-06 14:22:49 +0500

    mariamjawaid.wordpress.com

---
+++ Lab01.java
@@ -0,0 +1,24 @@
+class Lab01{
+public static void main(String args[]){
+String name="Amna";
+int omarks;
+omarks=825;
+float percent;
+percent=75;
+int tmarks;
+tmarks=1100;
+char grade;
+grade='A';
+boolean status=true;
+System.out.println("\nname=");
+System.out.println(name);
+System.out.println("\ntotal marks=");System.out.println(tmarks);System.out.println("\nobtained marks=");
+System.out.println(omarks);
+System.out.println("\nPercentage=");
+System.out.println(percent);
+System.out.println("\ngrade=");
+System.out.println(grade);
+System.out.println("\npass=");
+System.out.println(status);
+}
+}
